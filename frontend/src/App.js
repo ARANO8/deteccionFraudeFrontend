2cfe2d65bca7737f680495f631ebc6f7433911f1
ARANO8 <alanarnez51@gmail.com>
2025-06-02 17:25:33 -0400

    arreglo react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Transaccion from "./pages/Transaccion";
 import Dashboard from "./pages/Dashboard";
 import Registro from "./pages/Registro";
